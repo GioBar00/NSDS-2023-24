374e92ccf8be8e598a47307d03a9ffe21491eadb
Giovanni Barbiero <38290406+GioBar00@users.noreply.github.com>
2023-11-12 05:07:42 +0100

    Added Akka test project with OtherMessage edit.

---
+++ akka/src/main/java/com/counter/CounterActor.java
@@ -0,0 +1,35 @@
+package com.counter;
+
+import akka.actor.AbstractActor;
+import akka.actor.Props;
+
+public class CounterActor extends AbstractActor {
+
+	private int counter;
+
+	public CounterActor() {
+		this.counter = 0;
+	}
+
+	@Override
+	public Receive createReceive() {
+		return receiveBuilder()
+				.match(SimpleMessage.class, this::onMessage)
+				.match(OtherMessage.class, this::onOtherMessage)
+				.build();
+	}
+
+	void onMessage(SimpleMessage msg) {
+		++counter;
+		System.out.println("Counter increased to " + counter);
+	}
+
+	void onOtherMessage(OtherMessage msg) {
+		System.out.println("Received other message");
+	}
+
+	static Props props() {
+		return Props.create(CounterActor.class);
+	}
+
+}
