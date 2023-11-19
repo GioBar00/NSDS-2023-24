8c1b0acf6abadca769b18154c1427eb10b21409e
Giovanni Barbiero <38290406+GioBar00@users.noreply.github.com>
2023-11-19 22:57:04 +0100

    added akka lab 2022

---
+++ akka/src/main/java/com/lab2022/solution/BatchMsg.java
@@ -0,0 +1,14 @@
+package com.lab2022.solution;
+
+public class BatchMsg {
+	
+	private boolean isOn;
+	
+	public BatchMsg(boolean isOn) {
+		this.isOn = isOn;
+	}
+
+	public boolean isOn() {
+		return isOn;
+	}
+}
