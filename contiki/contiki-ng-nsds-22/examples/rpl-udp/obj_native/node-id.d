9d01b358c81bbd6fe8eb4c4dc1efc9f1d34c5c11
Giovanni Barbiero <38290406+GioBar00@users.noreply.github.com>
2023-12-18 15:32:48 +0100

    added contiki repo prof

---
+++ contiki/contiki-ng-nsds-22/examples/rpl-udp/obj_native/node-id.d
@@ -0,0 +1,22 @@
+obj_native/node-id.o: ../../os/sys/node-id.c ../../os/contiki.h \
+  ../../arch/platform/native/./contiki-conf.h \
+  ../../arch/cpu/native/./native-def.h ../../os/contiki-default-conf.h \
+  ../../os/sys/process.h ../../os/sys/pt.h ../../os/sys/lc.h \
+  ../../os/sys/lc-switch.h ../../os/sys/cc.h ../../os/sys/cc-gcc.h \
+  ../../os/sys/autostart.h ../../os/sys/timer.h ../../os/sys/clock.h \
+  ../../os/sys/ctimer.h ../../os/sys/etimer.h ../../os/sys/rtimer.h \
+  ../../arch/cpu/native/./rtimer-arch.h ../../os/sys/node-id.h \
+  ../../os/net/linkaddr.h ../../os/services/deployment/deployment.h \
+  ../../os/net/ipv6/uip.h ../../os/net/ipv6/uipopt.h \
+  ../../os/net/ipv6/uipbuf.h ../../os/net/ipv6/tcpip.h
+../../os/sys/node-id.c ../../os/contiki.h :
+  ../../arch/platform/native/./contiki-conf.h :
+  ../../arch/cpu/native/./native-def.h ../../os/contiki-default-conf.h :
+  ../../os/sys/process.h ../../os/sys/pt.h ../../os/sys/lc.h :
+  ../../os/sys/lc-switch.h ../../os/sys/cc.h ../../os/sys/cc-gcc.h :
+  ../../os/sys/autostart.h ../../os/sys/timer.h ../../os/sys/clock.h :
+  ../../os/sys/ctimer.h ../../os/sys/etimer.h ../../os/sys/rtimer.h :
+  ../../arch/cpu/native/./rtimer-arch.h ../../os/sys/node-id.h :
+  ../../os/net/linkaddr.h ../../os/services/deployment/deployment.h :
+  ../../os/net/ipv6/uip.h ../../os/net/ipv6/uipopt.h :
+  ../../os/net/ipv6/uipbuf.h ../../os/net/ipv6/tcpip.h :
---
+++ contiki/contiki-ng-nsds-22/examples/rpl-udp/obj_native/xmem.d
@@ -0,0 +1,16 @@
+obj_native/xmem.o: ../../arch/platform/native/dev/xmem.c \
+  ../../os/contiki.h ../../arch/platform/native/./contiki-conf.h \
+  ../../arch/cpu/native/./native-def.h ../../os/contiki-default-conf.h \
+  ../../os/sys/process.h ../../os/sys/pt.h ../../os/sys/lc.h \
+  ../../os/sys/lc-switch.h ../../os/sys/cc.h ../../os/sys/cc-gcc.h \
+  ../../os/sys/autostart.h ../../os/sys/timer.h ../../os/sys/clock.h \
+  ../../os/sys/ctimer.h ../../os/sys/etimer.h ../../os/sys/rtimer.h \
+  ../../arch/cpu/native/./rtimer-arch.h ../../os/dev/xmem.h
+../../arch/platform/native/dev/xmem.c :
+  ../../os/contiki.h ../../arch/platform/native/./contiki-conf.h :
+  ../../arch/cpu/native/./native-def.h ../../os/contiki-default-conf.h :
+  ../../os/sys/process.h ../../os/sys/pt.h ../../os/sys/lc.h :
+  ../../os/sys/lc-switch.h ../../os/sys/cc.h ../../os/sys/cc-gcc.h :
+  ../../os/sys/autostart.h ../../os/sys/timer.h ../../os/sys/clock.h :
+  ../../os/sys/ctimer.h ../../os/sys/etimer.h ../../os/sys/rtimer.h :
+  ../../arch/cpu/native/./rtimer-arch.h ../../os/dev/xmem.h :
